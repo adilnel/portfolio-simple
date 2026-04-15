20a4d25cd90da93c5aad85868e05be6a6d89cce1
adilnel <adilnel@gmail.com>
2026-04-16 01:02:13 +0300

    Add Google Analytics integration using @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import KeyboardNavigation from "@/components/KeyboardNavigation";
 import "./globals.css";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <KeyboardNavigation />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-2T090FHWFE" />
     </html>
   );
 }
